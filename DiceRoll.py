151ab1e953f4f05998fd366028a9ea8f4761612f
skhairlight <skhairlight@gmail.com>
2025-10-05 17:11:37 -0500

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,17 +1,26 @@
 #DiceRoll.py
-#Name:
-#Date:
-#Assignment:
+#Name: Salsabiel Khair Allah
+#Date: Oct.5
+#Assignment: Lab 6
 import random
 
 def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+  for i in range(10000):
+    die1 = random.randit(1, 6)
+    die2 = random.randit(1, 6)
+    total = die1+ die2
+    rolls[total - 1] += 1  
   #find the sum total of the two dice
   
   #print statictics for dice rolls
+  print("Total\tCount\tPercentage")
+  for i in range(2,13):
+    count = rolls[i - 1]
+    percent = (count / 10000) * 100
+    print(f"{i}\t{count}\t{percent:.2f}%")
 
 
 if __name__ == '__main__':
