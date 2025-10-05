3512edfbdae55179cc74e9a206ecfa92cf36cc54
skhairlight <skhairlight@gmail.com>
2025-10-05 17:21:46 -0500

    Update WordIndex.py

--- WordIndex.py
+++ WordIndex.py
@@ -1,20 +1,34 @@
 #WordIndex.py
-#Name:
-#Date:
-#Assignment:
+#Name: Salsabiel Khair Allah
+#Date: Oct.5
+#Assignment: Lab 6
 
 def main():
   textFile = open("fish.txt", 'r')
   
   words = {} #create an empty dictionary
-  
-  
-  print ("fish" in words) #is a word already in the dictionary?
-  words["fish"] = [2]     #add a list to the dictionary
-  print ("fish" in words) #is the word there now?
-  words["fish"].append(5) #add to an existing list
-  print(words)
 
+  lineNum = 0
+  for line in textFile:
+    lineNum += 1
+    lineWords = line.strip().split()
+
+    for word in lineWords:
+      word = word.strip(".,!?;:\"'()[]{}").lower()
+      if word == "":
+        continue
+
+      if word not in words:
+        words[word] = [lineNum]
+      else:
+        if lineNum not in words[word]:
+          words[word].append(lineNum)
+
+  textFile.close()
+
+  for word in sorted(words):
+    print(word, ":", words[word])
+    
 
 if __name__ == '__main__':
   main()
